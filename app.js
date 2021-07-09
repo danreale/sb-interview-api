c6df84b83d39a5aabf2c69b1b4ecffb4a4661b1c
DanReale <dan.reale@semanticbits.com>
2021-07-08 23:19:50 -0400

    added api

---
+++ app.js
@@ -0,0 +1,185 @@
+require("dotenv").config();
+const express = require("express");
+
+const createClient = require("@supabase/supabase-js").createClient;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let port = process.env.PORT || 3000;
+
+app.get("/api/v1/health", (req, res) =>
+  res.send({ message: "API Is Running" })
+);
+
+app.get("/api/v1/issues", async (req, res) => {
+  if (req.query.category == undefined && req.query.status == undefined) {
+    await getAllIssuesNoFilter(res);
+  } else {
+    if (req.query.status == undefined) {
+      await getAllIssuesByCategory(req, res);
+    } else if (req.query.category == undefined) {
+      await getAllIssuesByStatus(req, res);
+    } else {
+      await getAllIssuesByStatusAndCategory(req, res);
+    }
+  }
+});
+app.get("/api/v1/issues/:id", async (req, res) => {
+  try {
+    let { data: issues, error } = await supabase
+      .from("issues")
+      .select("*")
+      .eq("id", req.params.id);
+    if (error) {
+      res.send({ message: "Query Error" });
+    } else {
+      if (issues.length == 0) {
+        res.send({ message: "No Matching Issues" });
+      } else {
+        res.send(issues);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+app.post("/api/v1/issues", async (req, res) => {
+  try {
+    const { data, error } = await supabase.from("issues").insert([
+      {
+        snowids: req.body.snowids,
+        date_reported: req.body.date_reported,
+        status: req.body.status,
+        category: req.body.category,
+        description: req.body.description,
+        date_resolved: req.body.date_resolved,
+        provider_types: req.body.provider_types,
+      },
+    ]);
+    if (error) {
+      res.send({ message: "Query Error" });
+    } else {
+      res.send(data);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+app.put("/api/v1/issues/:id", async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from("issues")
+      .update({
+        snowids: req.body.snowids,
+        date_reported: req.body.date_reported,
+        status: req.body.status,
+        category: req.body.category,
+        description: req.body.description,
+        date_resolved: req.body.date_resolved,
+        provider_types: req.body.provider_types,
+      })
+      .eq("id", req.params.id);
+    if (error) {
+      res.send({ message: "Query Error" });
+    } else {
+      res.send(data);
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+app.listen(port, function () {
+  console.log("Running API on Port " + port);
+});
+
+module.exports = app; // for testing
+
+async function getAllIssuesNoFilter(res) {
+  try {
+    let { data: issues, error } = await supabase.from("issues").select("*");
+    if (error) {
+      res.send({ message: "Query Error" });
+    } else {
+      if (issues.length == 0) {
+        res.send({ message: "No Matching Issues" });
+      } else {
+        res.send(issues);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+}
+async function getAllIssuesByStatus(req, res) {
+  try {
+    let { data: issues, error } = await supabase
+      .from("issues")
+      .select("*")
+      .eq("status", req.query.status);
+    if (error) {
+      res.send({ message: "Query Error" });
+    } else {
+      if (issues.length == 0) {
+        res.send({ message: "No Matching Issues" });
+      } else {
+        res.send(issues);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+}
+async function getAllIssuesByCategory(req, res) {
+  try {
+    let { data: issues, error } = await supabase
+      .from("issues")
+      .select("*")
+      .eq("category", req.query.category);
+    if (error) {
+      res.send({ message: "Query Error" });
+    } else {
+      if (issues.length == 0) {
+        res.send({ message: "No Matching Issues" });
+      } else {
+        res.send(issues);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+}
+async function getAllIssuesByStatusAndCategory(req, res) {
+  try {
+    let { data: issues, error } = await supabase
+      .from("issues")
+      .select("*")
+      .eq("status", req.query.status)
+      .eq("category", req.query.category);
+    if (error) {
+      res.send({ message: "Query Error" });
+    } else {
+      if (issues.length == 0) {
+        res.send({ message: "No Matching Issues" });
+      } else {
+        res.send(issues);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+}
---
+++ tests/api.spec.js
@@ -0,0 +1,118 @@
+const chai = require("chai");
+const expect = chai.expect;
+const chaiHttp = require("chai-http");
+const server = require("../app");
+
+chai.use(chaiHttp);
+const appServer = chai.request(server).keepOpen();
+
+let issue_id;
+
+describe("API Tests", () => {
+  after(() => {
+    appServer.close();
+  });
+  it("Health Check", (done) => {
+    appServer.get("/api/v1/health").end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res.body.message).to.eql("API Is Running");
+      done();
+    });
+  });
+  it("Get All Issues", (done) => {
+    appServer.get("/api/v1/issues").end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res.body).to.have.length.gte(1);
+      done();
+    });
+  });
+  it("Get All General Issues", (done) => {
+    appServer.get("/api/v1/issues?category=General").end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res.body).to.have.lengthOf(1);
+      done();
+    });
+  });
+  it("Get All Open Issues", (done) => {
+    appServer.get("/api/v1/issues?status=Open").end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res.body).to.have.lengthOf(1);
+      done();
+    });
+  });
+  it("Get All General Open Issues", (done) => {
+    appServer
+      .get("/api/v1/issues?category=General&status=Open")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.lengthOf(1);
+        done();
+      });
+  });
+  it("Get Specific Issue", (done) => {
+    appServer.get("/api/v1/issues/1").end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res.body).to.have.lengthOf(1);
+      expect(res.body[0].snowids).to.eql(["1", "2", "3"]);
+      expect(res.body[0].date_reported).to.eql("2021-07-09T01:01:55+00:00");
+      expect(res.body[0].status).to.eql("Open");
+      expect(res.body[0].category).to.eql("General");
+      expect(res.body[0].description).to.eql(
+        "User Information, such as email address and last name, on the My Profile page cannot be updated. User has already updated Profile information in HARP."
+      );
+      expect(res.body[0].date_resolved).to.eql("2021-07-09T01:01:55+00:00");
+      expect(res.body[0].provider_types).to.eql(["HHA", "IRF", "LTCH"]);
+      done();
+    });
+  });
+  it("Add New Issue", (done) => {
+    appServer
+      .post("/api/v1/issues")
+      .send({
+        snowids: ["222", "333", "444"],
+        date_reported: "2021-06-21",
+        status: "Open",
+        category: "Survey And Certification",
+        description: "This is an API test.",
+        date_resolved: null,
+        provider_types: ["HHA", "IRF", "LTCH"],
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        issue_id = res.body[0].id;
+        expect(res.body[0].snowids).to.eql(["222", "333", "444"]);
+        expect(res.body[0].date_reported).to.eql("2021-06-21T00:00:00+00:00");
+        expect(res.body[0].status).to.eql("Open");
+        expect(res.body[0].category).to.eql("Survey And Certification");
+        expect(res.body[0].description).to.eql("This is an API test.");
+        expect(res.body[0].date_resolved).to.eql(null);
+        expect(res.body[0].provider_types).to.eql(["HHA", "IRF", "LTCH"]);
+        done();
+      });
+  });
+  it("Update Issue", (done) => {
+    appServer
+      .put(`/api/v1/issues/${issue_id}`)
+      .send({
+        snowids: ["222", "333", "444"],
+        date_reported: "2021-06-21",
+        status: "Resolved",
+        category: "Survey And Certification",
+        description: "This is an API test.",
+        date_resolved: "2021-06-25",
+        provider_types: ["HHA", "IRF", "LTCH"],
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body[0].snowids).to.eql(["222", "333", "444"]);
+        expect(res.body[0].date_reported).to.eql("2021-06-21T00:00:00+00:00");
+        expect(res.body[0].category).to.eql("Survey And Certification");
+        expect(res.body[0].description).to.eql("This is an API test.");
+        expect(res.body[0].provider_types).to.eql(["HHA", "IRF", "LTCH"]);
+        expect(res.body[0].status).to.eql("Resolved");
+        expect(res.body[0].date_resolved).to.eql("2021-06-25T00:00:00+00:00");
+        expect(res.body[0].id).to.eql(issue_id);
+        done();
+      });
+  });
+});
